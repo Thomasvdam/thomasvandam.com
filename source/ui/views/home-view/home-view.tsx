f461e004fe7e2fe7aeb85aae82be9d0fb90283f4
Thomas van Dam <t.vandam@wearetriple.com>
2020-12-06 17:22:21 +0100

    fix: Font sizes are applied correctly now

--- source/ui/views/home-view/home-view.tsx
+++ source/ui/views/home-view/home-view.tsx
@@ -5,7 +5,7 @@ import $ from './home-view.module.scss';
 const HomeView: React.FC = () => {
     return (
         <div className={$.home}>
-            <h1>This is my face</h1>
+            <h1 className={$.home_title}>This is my face</h1>
             <Image src="/images/catman.jpeg" alt="Who could it be?" height="300" width="300" />
             <p>Maybe I&apos;ll snazz it up in the future, who knows?</p>
         </div>
