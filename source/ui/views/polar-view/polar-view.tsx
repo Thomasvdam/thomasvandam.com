c79556ad59002df2018140fc4a538d019bc32d99
Thomas van Dam <t.vandam@wearetriple.com>
2020-12-06 21:52:24 +0100

    feat: Add description section to polar-bears

--- source/ui/views/polar-view/polar-view.tsx
+++ source/ui/views/polar-view/polar-view.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { d6 } from '~source/core/models';
-import { Die6, PageContainer } from '~source/ui/components/atoms';
+import { Die6, Expander, PageContainer } from '~source/ui/components/atoms';
 import $ from './polar-view.module.scss';
 
 interface iProps {
@@ -27,11 +27,24 @@ const PolarView: React.FC<iProps> = ({ diceRolls, nextRoll }) => {
         <div className={$.polar}>
             <PageContainer className={$.polar_container}>
                 <h1>Ice Holes and Polar Bears</h1>
+                <section>
+                    <p>Today&apos;s view of the Arctic is rather pretty don&apos;t you think?</p>
+                </section>
                 <section className={$.dice_container}>
                     {diceRolls.map((die, index) => (
                         // eslint-disable-next-line react/no-array-index-key
                         <Die6 key={index} angled colour={die.colour} number={die.roll} />
                     ))}
+                </section>
+                <section>
+                    <Expander
+                        className={$.description_expander}
+                        title={<h4 className={$.description_title}>What is this about?</h4>}
+                    >
+                        <p className={$.description_text}>
+                            \// TODO: A short text about the riddle.
+                        </p>
+                    </Expander>
                 </section>
                 <section className={$.next_roll_container}>
                     Approximately {hours} {hourWord} until the next roll.
